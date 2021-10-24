bdff9077b7bf1333a09dd57f0b28cad95e58134c
Georg Popp <Georg.Popp@sovanta.com>
2021-10-25 00:27:51 +0200

    Add Search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -18,11 +19,21 @@ class App extends Component {
     this.setState({ users: res.data, loading: false} );
   }
 
+  // Search Github Users
+  searchUsers = async (text) => {
+    this.setState({ loading: true});
+    
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}`);
+
+    this.setState({ users: res.data.items, loading: false} );
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users}/> 
         </div>
       </div>
