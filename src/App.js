486a3ad40bc42a9c5692f46dee9a1dc774c7c0a5
Georg Popp <Georg.Popp@sovanta.com>
2021-10-24 23:24:43 +0200

    Refactor App
    Added ';'
    delete empty lines of code

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        
         <div className="container">
           <Users /> 
         </div>
       </div>
     )
   }
-}
+};
 
 export default App;
