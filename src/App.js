f1c47a2dc6fdb611e6268acf1b071ac9a4246d82
Georg Popp <Georg.Popp@sovanta.com>
2021-10-25 13:16:09 +0200

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -6,48 +6,50 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-  };
+	state = {
+		users: [],
+		loading: false,
+	};
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+	async componentDidMount() {
+		this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users');
+		const res = await axios.get('https://api.github.com/users');
 
-    this.setState({ users: res.data, loading: false });
-  }
+		this.setState({ users: res.data, loading: false });
+	}
 
-  // Search Github Users
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
+	// Search Github Users
+	searchUsers = async (text) => {
+		this.setState({ loading: true });
 
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}`,
-    );
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}`
+		);
 
-    this.setState({ users: res.data.items, loading: false });
-  };
+		this.setState({ users: res.data.items, loading: false });
+	};
 
-  // Clear users from state
-  clearUsers = () => this.setState({ users: [], loading: false });
+	// Clear users from state
+	clearUsers = () => this.setState({ users: [], loading: false });
 
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
-          />
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const { users, loading } = this.state;
+
+		return (
+			<div className='App'>
+				<Navbar />
+				<div className='container'>
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={users.length > 0 ? true : false}
+					/>
+					<Users loading={loading} users={users} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
