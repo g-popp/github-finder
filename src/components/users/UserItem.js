95eb45a2cd26aad0ee721a31adf9caa12e1d3fa9
Georg Popp <Georg.Popp@sovanta.com>
2021-10-25 11:57:00 +0200

    Formatting

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const UserItem = ({user: {login, avatar_url, html_url}}) => {
-    return (
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div>
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
         <div>
-            <div className="card text-center">
-                <img
-                src={avatar_url}
-                alt=""
-                className="round-img" 
-                style={{width: '60px'}}/>
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">
-                        More
-                    </a>
-                </div>
-            </div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 UserItem.propTypes = {
-    user: PropTypes.object.isRequired,
-}
+  user: PropTypes.object.isRequired,
+};
 
-export default UserItem
+export default UserItem;
