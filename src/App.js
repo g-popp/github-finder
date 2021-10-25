5d7cc135ef16961e67bd5ada566b4e15f5ff2d41
Georg Popp <Georg.Popp@sovanta.com>
2021-10-25 11:56:05 +0200

    Add clear button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
@@ -8,37 +8,46 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
-  }
+    loading: false,
+  };
 
   async componentDidMount() {
-    this.setState({ loading: true});
+    this.setState({ loading: true });
 
     const res = await axios.get('https://api.github.com/users');
 
-    this.setState({ users: res.data, loading: false} );
+    this.setState({ users: res.data, loading: false });
   }
 
   // Search Github Users
   searchUsers = async (text) => {
-    this.setState({ loading: true});
-    
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}`);
+    this.setState({ loading: true });
 
-    this.setState({ users: res.data.items, loading: false} );
-  }
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}`,
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users}/> 
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default App;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,33 +1,53 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
-    state = {
-        text: ''
-    };
+  state = {
+    text: '',
+  };
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-    }
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
 
-    onSubmit = (event) => {
-        event.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' })
-    }
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
 
-    onChange = (event) => this.setState({ [event.target.name]: event.target.value });
-    
-    render() {
-        return (
-            <div>
-                <form className="form" onSubmit={this.onSubmit}>
-                    <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange}/>
-                    <input type="submit" value="Search" className="btn btn-dark btn-block" />    
-                </form>                
-            </div>
-        )
-    }
+  onChange = (event) =>
+    this.setState({ [event.target.name]: event.target.value });
+
+  render() {
+    const { showClear, clearUsers } = this.props;
+
+    return (
+      <div>
+        <form className="form" onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
