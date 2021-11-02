665c53a0f04acca97f9c25ae577cdfbaa3ee087a
Georg Popp <Georg.Popp@sovanta.com>
2021-11-02 23:41:57 +0100

    Add Users to Home Page after load

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
+import GithubContext from '../../context/github/githubContext';
 import Search from '../users/Search';
 import Users from '../users/Users';
 
 const Home = () => {
+	const githubContext = useContext(GithubContext);
+
+	const { getUsers } = githubContext;
+
+	useEffect(() => {
+		getUsers();
+		// eslint-disable-next-line
+	}, []);
+
 	return (
 		<Fragment>
 			<Search />
